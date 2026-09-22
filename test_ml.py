520c48a7c9a7f89fb590ac64f9110271c33f1d88
bob-student-DA <bhickm13@wgu.edu>
2026-09-22 22:35:22 +0000

    Add unit tests, passing test screenshot, updated gitignore

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,37 @@
 import pytest
-# TODO: add necessary import
-
-# TODO: implement the first test. Change the function name and input as needed
-def test_one():
-    """
-    # add description for the first test
-    """
-    # Your code here
-    pass
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from ml.model import train_model, inference, compute_model_metrics
 
 
-# TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_train_model():
     """
-    # add description for the second test
+    Test that train_model returns a RandomForestClassifier instance.
     """
-    # Your code here
-    pass
+    X_train = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
+    y_train = np.array([0, 1, 0, 1])
+    model = train_model(X_train, y_train)
+    assert isinstance(model, RandomForestClassifier)
+
+def test_inference():
+    """
+    Test that inference returns predictions with the same number of rows as the input.
+    """
+    X_train = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
+    y_train = np.array([0, 1, 0, 1])
+    model = train_model(X_train, y_train)
+    preds = inference(model, X_train)
+    assert len(preds) == len(y_train)
 
 
-# TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_compute_model_metrics():
     """
-    # add description for the third test
+    Test that compute_model_metrics returns correct precision, recall, and F1
+    on a known, hand-calculated example.
     """
-    # Your code here
-    pass
+    y_true = np.array([1, 1, 0, 0])
+    y_preds = np.array([1, 0, 0, 0])
+    precision, recall, fbeta = compute_model_metrics(y_true, y_preds)
+    assert precision == 1.0
+    assert recall == 0.5
+    assert fbeta == pytest.approx(0.6667, abs=0.001)
