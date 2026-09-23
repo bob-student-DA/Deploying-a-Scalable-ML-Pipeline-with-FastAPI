a51f2f621a7be9e65bf948b79b0c80b3c8e12132
bob-student-DA <bhickm13@wgu.edu>
2026-09-23 17:27:34 +0000

    Fix main.py flake8 issues and finalize enironment.yml

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from pydantic import BaseModel, Field
 from ml.data import apply_label, process_data
 from ml.model import inference, load_model
 
+
 # DO NOT MODIFY
 class Data(BaseModel):
     age: int = Field(..., example=37)
@@ -24,7 +25,10 @@ class Data(BaseModel):
     capital_gain: int = Field(..., example=0, alias="capital-gain")
     capital_loss: int = Field(..., example=0, alias="capital-loss")
     hours_per_week: int = Field(..., example=40, alias="hours-per-week")
-    native_country: str = Field(..., example="United-States", alias="native-country")
+    native_country: str = Field(
+        ..., example="United-States", alias="native-country"
+    )
+
 
 path = os.path.join("model", "encoder.pkl")
 encoder = load_model(path)
@@ -44,11 +48,15 @@ async def get_root():
 
 @app.post("/data/")
 async def post_inference(data: Data):
-    # DO NOT MODIFY: turn the Pydantic model into a dict.
+    # DO NOT MODIFY: turn the Pydantic
+    # model into a dict.
     data_dict = data.dict()
-    # DO NOT MODIFY: clean up the dict to turn it into a Pandas DataFrame.
-    # The data has names with hyphens and Python does not allow those as variable names.
-    # Here it uses the functionality of FastAPI/Pydantic/etc to deal with this.
+    # DO NOT MODIFY: clean up the dict to
+    # turn it into a Pandas DataFrame.
+    # The data has names with hyphens and
+    # Python does not allow those as variable names.
+    # Here it uses the functionality of
+    # FastAPI/Pydantic/etc to deal with this.
     data = {k.replace("_", "-"): [v] for k, v in data_dict.items()}
     data = pd.DataFrame.from_dict(data)
 
@@ -63,14 +71,12 @@ async def post_inference(data: Data):
         "native-country",
     ]
     data_processed, _, _, _ = process_data(
-    data,
-    categorical_features=cat_features,
-    label=None,
-    training=False,
-    encoder=encoder,
-    lb=None,
-)
-    
-
+        data,
+        categorical_features=cat_features,
+        label=None,
+        training=False,
+        encoder=encoder,
+        lb=None,
+    )
     _inference = inference(model, data_processed)
     return {"result": apply_label(_inference)}
